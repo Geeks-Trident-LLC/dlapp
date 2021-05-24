eaf6c70653f78ef768d97b03211919afe92b1caf
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-05-23 21:53:15 -0700

    add DLQuery.__getitem__ special method

--- dlquery/dlquery.py
+++ dlquery/dlquery.py
@@ -35,6 +35,9 @@ class DLQuery:
     def __len__(self):
         return len(self.data)
 
+    def __getitem__(self, item):
+        return self.data[item]
+
     ############################################################################
     # properties
     ############################################################################
