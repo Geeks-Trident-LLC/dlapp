fb32d48bd530a90e4b53cff513743fed1cbf36b8
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-06-01 14:40:23 -0700

    implement dlquery.main.Cli.validate_filename

--- dlquery/main.py
+++ dlquery/main.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+from os import path
 
 
 def show_tutorial_dlquery():
@@ -28,6 +29,7 @@ class Cli:
     def __init__(self):
         self.filename = ''
         self.filetype = ''
+        self.result = None
 
         parser = argparse.ArgumentParser(
             prog='dlquery',
@@ -36,7 +38,7 @@ class Cli:
         )
 
         parser.add_argument(
-            '--app', action='store_true', dest='app',
+            '--application', action='store_true',
             help='launch a dlquery GUI application'
         )
 
@@ -47,7 +49,7 @@ class Cli:
         )
 
         parser.add_argument(
-            '--filetype', type=str, choices=['json', 'yaml', 'yml', 'csv'],
+            '--filetype', type=str, choices=['csv', 'json', 'yaml', 'yml'],
             default='',
             help='a file type can be either json, yaml, yml, or csv'
         )
@@ -66,53 +68,106 @@ class Cli:
 
         parser.add_argument(
             '--tutorial', action='store_true', dest='tutorial',
-            help='show DLQuery tutorial'
+            help='show dlquery tutorial'
         )
 
         parser.add_argument(
             '--tutorial-csv', action='store_true', dest='tutorial_csv',
-            help='show JSON tutorial'
+            help='show csv tutorial'
         )
 
         parser.add_argument(
             '--tutorial-json', action='store_true', dest='tutorial_json',
-            help='show JSON tutorial'
+            help='show json tutorial'
         )
 
         parser.add_argument(
             '--tutorial-yaml', action='store_true', dest='tutorial_yaml',
-            help='show JSON tutorial'
+            help='show yaml tutorial'
         )
 
         self.parser = parser
 
-    def validate_filename(self, filename, filetype):
-        raise NotImplementedError('TODO: Need to implement validate_filename')
+    @property
+    def is_csv_type(self):
+        """Return True if filetype is csv"""
+        return self.filetype == 'csv'
+
+    @property
+    def is_json_type(self):
+        """Return True if filetype is json"""
+        return self.filetype == 'json'
+
+    @property
+    def is_yaml_type(self):
+        """Return True if filetype is yml or yaml"""
+        return self.filetype in ['yml', 'yaml']
+
+    def validate_filename(self, options):
+        """Validate `filename` flag which is a file type of `csv`,
+        `json`, `yml`, or `yaml`
+
+        Parameters
+        ----------
+        options (argparse.Namespace): a argparse.Namespace instance.
+
+        Returns
+        -------
+        bool: True if `filename` is valid, otherwise, ``sys.exit(1)``
+        """
+        filename, filetype = str(options.filename), str(options.filetype)
+        if not filename:
+            print('*** --filename flag CAN NOT be empty.')
+            sys.exit(1)
+
+        self.filename = filename
+        self.filetype = filetype
+
+        _, ext = path.splitext(filename)
+        ext = ext.lower()
+        if ext in ['.csv', '.json', '.yml', '.yaml']:
+            self.filetype = ext[1:]
+            return True
+
+        if not filetype:
+            if ext == '':
+                fmt = ('*** {} file doesnt have an extension.  '
+                       'System cant determine a file type.  '
+                       'Please rerun with --filetype=<filetype> '
+                       'where filetype is csv, json, yml, or yaml.')
+
+            else:
+                fmt = ('*** {} file has an extension but it is not '
+                       'csv, json, yml, or yaml.  If you think this file is'
+                       'csv, json, yml, or yaml, '
+                       'please rerun with --filetype=<filetype> '
+                       'where filetype is csv, json, yml, or yaml.')
+            print(fmt.format(filename))
+            sys.exit(1)
+        else:
+            self.filetype = filetype
 
     def run(self):
-        args = self.parser.parse_args()
-        chk = any(bool(i) for i in vars(args).values())
+        """Take CLI arguments, parse it, and process."""
+        options = self.parser.parse_args()
+        chk = any(bool(i) for i in vars(options).values())
 
         if not chk:
             self.parser.print_help()
             sys.exit(1)
 
-        if args.tutorial or args.tutorial_csv or args.tutorial_json or args.tutorial_yaml:
-            if args.tutorial:
+        if options.tutorial or options.tutorial_csv or options.tutorial_json or options.tutorial_yaml:
+            if options.tutorial:
                 show_tutorial_dlquery()
-            if args.tutorial_csv:
+            if options.tutorial_csv:
                 show_tutorial_csv()
-            if args.tutorial_json:
+            if options.tutorial_json:
                 show_tutorial_json()
-            if args.tutorial_yaml:
+            if options.tutorial_yaml:
                 show_tutorial_yaml()
             sys.exit(0)
 
-        if not args.filename:
-            print('*** --filename flag CAN NOT be empty.')
-            sys.exit(1)
-
-        self.validate_filename(args.filename, args.filetype)
+        self.validate_filename(options)
 
 
 def execute():
