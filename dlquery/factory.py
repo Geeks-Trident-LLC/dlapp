fa4c97f174ff5472ba7c0380f0af3c0d45cdf22c
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-06-01 11:37:39 -0700

    add dlquery.factory.create_from_csv_file and dlquery.factory.create_from_csv_data functions

--- dlquery/factory.py
+++ dlquery/factory.py
@@ -2,6 +2,7 @@
 
 import yaml
 import json
+import csv
 from dlquery import DLQuery
 
 
@@ -59,4 +60,54 @@ def create_from_yaml_data(data, loader=yaml.SafeLoader):
     """
     obj = yaml.load(data, Loader=loader)
     query_obj = DLQuery(obj)
+    return query_obj
+
+
+def create_from_csv_file(filename, fieldnames=None, restkey=None,
+                         restval=None, dialect='excel', *args, **kwds):
+    """Create a dlquery instance from CSV file.
+    Parameters:
+        filename (str): a CSV file.
+        fieldnames (list): list of keys for the dict.
+        restkey (str): key to catch long rows.
+        restval (Any): default value for short rows.
+        dialect (str): a CSV dialect.  Default is excel.
+        args (tuple): any argument for csv.DictReader.
+        kwds (dict): any keyword argument for csv.DictReader.
+    Return:
+        DLQuery: a DLQuery instance.
+    """
+    with open(filename, newline='') as stream:
+        csv_reader = csv.DictReader(
+            stream, fieldnames=fieldnames, restkey=restkey,
+            restval=restval, dialect=dialect, *args, **kwds
+        )
+        lst_of_dict = [row for row in csv_reader]
+        query_obj = DLQuery(lst_of_dict)
+        return query_obj
+
+
+def create_from_csv_data(data, fieldnames=None, restkey=None,
+                         restval=None, dialect='excel', *args, **kwds):
+    """Create a dlquery instance from CSV data.
+    Parameters:
+        data (str): a CSV data.
+        fieldnames (list): list of keys for the dict.
+        restkey (str): key to catch long rows.
+        restval (Any): default value for short rows.
+        dialect (str): a CSV dialect.  Default is excel.
+        args (tuple): any argument for csv.DictReader.
+        kwds (dict): any keyword argument for csv.DictReader.
+    Return:
+        DLQuery: a DLQuery instance.
+    """
+    from io import StringIO
+    data = str(data).strip()
+    stream = StringIO(data)
+    csv_reader = csv.DictReader(
+        stream, fieldnames=fieldnames, restkey=restkey,
+        restval=restval, dialect=dialect, *args, **kwds
+    )
+    lst_of_dict = [row for row in csv_reader]
+    query_obj = DLQuery(lst_of_dict)
     return query_obj
