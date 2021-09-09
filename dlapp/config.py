ddc7f87744431d10bd8fbef6c201daad91d4a593
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-09-09 01:11:33 -0700

    baseline: fix Data.repo_url typo

--- dlapp/config.py
+++ dlapp/config.py
@@ -38,7 +38,7 @@ class Data:
     company_url = 'https://www.geekstrident.com/'
 
     # URL
-    repo_url = 'https://github.com/Geeks-Trident-LLC/regexapp'
+    repo_url = 'https://github.com/Geeks-Trident-LLC/dlapp'
     # TODO: Need to update wiki page for documentation_url instead of README.md.
     documentation_url = path.join(repo_url, 'blob/develop/README.md')
     license_url = path.join(repo_url, 'blob/develop/LICENSE')
