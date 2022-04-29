933f9032c102e71e0f0010aa0e74ef9633811efc
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-04-29 14:35:22 -0700

    version: update dlapp to version 0.3.6

--- dlapp/config.py
+++ dlapp/config.py
@@ -7,7 +7,7 @@ import compare_versions
 import dateutil
 import yaml
 
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 version = __version__
 __edition__ = 'Community'
 edition = __edition__
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dlapp',
-    version='0.3.5',
+    version='0.3.6',
     license='BSD-3-Clause',
     license_files=['LICENSE'],
     description='Python module for querying dictionary or list object.',
